256f3598b1ac6ea97f9aeba5a43372244ceacef7
Rarysson Guilherme <r_guilherme12@hotmail.com>
2022-04-18 11:58:31 -0300

    Melhorado design do carrinho

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
 
+interface CartItem {
+  name: string;
+  qnt: number;
+  price: number;
+}
+
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
@@ -11,7 +17,30 @@ export class TopBarComponent implements OnInit {
 
   ngOnInit(): void { }
 
-  totalItensOnCart() {
+  totalItemsOnCart(): number {
     return this.cartService.getItems().length;
+  }
+
+  itemsOnCart(): CartItem[] {
+    const items = this.cartService.getItems().reduce((acc, value) => {
+      if (!acc[value.name]) {
+        acc[value.name] = {
+          name: value.name,
+          qnt: 1,
+          price: value.price
+        }
+      } else {
+        acc[value.name].qnt++;
+        acc[value.name].price += value.price;
+      }
+
+      return acc;
+    }, {} as Record<string, CartItem>);
+
+    return Object.values(items);
+  }
+
+  totalPrice(): number {
+    return this.itemsOnCart().reduce((acc, value) => acc + value.price, 0);
   }
 }
